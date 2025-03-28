b54a139d20a0e4d8254db5eb5859baeed41fb1a3
RetroR1xE <118540595+RetroR1xE@users.noreply.github.com>
2025-03-28 00:04:19 -0300

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,11 +1,11 @@
-/***************************************
-*Demo Program 
+/******************************************************************
+*Demo Program (This section will contain the code of the project)
 *CS-1103 Project Code 
-******************************************/
+*******************************************************************/
 public class Demo
 {
     public static void main(String[] args)
     {
-        System.out.println("Wsg gang?!");
+        System.out.println("Hello Dr. Kim!");
     }
 }
