9c796edd51ea7bbc0280d69b2818b9ac75d36e43
RetroR1xE <118540595+RetroR1xE@users.noreply.github.com>
2025-03-27 23:50:23 -0300

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+/***************************************
+*Demo Program 
+*CS-1103 Project Code 
+******************************************/
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Wsg gang?!");
+    }
+}
